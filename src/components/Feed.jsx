14cf9f056d2360a95111fe192f2fc8d7f68c04ca
Tigran <tigran2589@gmail.com>
2023-05-03 18:37:46 +0300

    Fixed year in the sidebar footer

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -30,7 +30,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: '#fff' }}
         >
-          Copyright © 2022 JSM Media
+          Copyright © {new Date().getFullYear()} JSM Media
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
